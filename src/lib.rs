a10fa428123d3686ff99aab860eaa5231a930b7e
Brendan Molloy <brendan@bbqsrc.net>
2021-03-20 11:41:08 +0100

    Fix documentation and metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,31 @@
+//! Compile-time validation of [`regex::Regex`](https://github.com/rust-lang/regex).
+//! 
+//! ## Examples
+//! 
+//! ### Lazy regex
+//! 
+//! Uses `once_cell` to lazily create the regex.
+//! 
+//! ```rust
+//! static RE: Lazy<Regex> = regex_static::lazy_regex!("^yesss$");
+//! ```
+//! 
+//! ### Static regex
+//! 
+//! Also uses `once_cell`, but works inline (will therefore reuse the same instance of the regex each function call).
+//! 
+//! ```rust
+//! let some_regex = regex_static::static_regex!("^yesss$");
+//! ```
+//! 
+//! ### Ordinary regex
+//! 
+//! Will create an owned `Regex`, just like calling `Regex::new(...)` but with compile-time validation.
+//! 
+//! ```rust
+//! let ordinary_regex = regex_static::regex!("^yesss$");
+//! ```
+
 pub use regex_static_macro::{lazy_regex, regex, static_regex};
 
 #[doc(hidden)]
